6ef5e3bc83b722114a2c7750af86544ef577d8b5
Shelgas <akozlov1997@gmail.com>
2024-02-07 20:38:23 +0300

    Implemented adding a card

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,28 +1,22 @@
-const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
-];
+const cardTemplate = document.querySelector("#card-template").content;
 
-export {initialCards};
+export const createCard = (item, deleteCard) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardTitle.textContent = item.name;
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  deleteButton.addEventListener("click", (evt) => {
+    deleteCard(evt);
+  });
+
+  return cardElement;
+};
+
+export const deleteCard = (evt) => {
+  const item = evt.target.closest(".card");
+  item.remove();
+};
--- src/index.js
+++ src/index.js
@@ -1,67 +1,80 @@
-import './pages/index.css';
-import { initialCards} from './components/cards';
-import { openModal, closeModal } from './components/modal';
+import "./pages/index.css";
+import { initialCards } from "./components/initialCards";
+import { openModal, closeModal } from "./components/modal";
+import { createCard, deleteCard } from "./components/cards";
 
-
-const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
-const popup = document.querySelector('.popup');
-const popupEdit = document.querySelector('.popup_type_edit');
-const popupCard = document.querySelector('.popup_type_new-card');
+const popup = document.querySelector(".popup");
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupCard = document.querySelector(".popup_type_new-card");
 
-const cardAddBtn = document.querySelector('.profile__add-button'); 
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const closeBtns = document.querySelectorAll('.popup__close');
+const cardAddBtn = document.querySelector(".profile__add-button");
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const closeBtns = document.querySelectorAll(".popup__close");
 
-const profileName = document.querySelector('.profile__title')
-const profileDescription = document.querySelector('.profile__description')
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-const profileEditForm = document.forms['edit-profile'];
+const profileEditForm = document.forms["edit-profile"];
 const profileInputName = profileEditForm.elements.name;
 const profileInputDesc = profileEditForm.elements.description;
 
-profileEditBtn.addEventListener('click', (evt) => {
+const cardForm = document.forms["new-place"];
+const cardInputName = cardForm.elements["place-name"];
+const cardInputLink = cardForm.elements.link;
+
+profileEditBtn.addEventListener("click", (evt) => {
   profileInputName.value = profileName.textContent;
   profileInputDesc.value = profileDescription.textContent;
   openModal(popupEdit);
 });
-cardAddBtn.addEventListener('click', () => openModal(popupCard));
+cardAddBtn.addEventListener("click", () => openModal(popupCard));
+profileEditForm.addEventListener("submit", handleEditFormSubmit);
+cardForm.addEventListener("submit", handleCardFormSubmit);
 
-Array.from(closeBtns).forEach((btn) => btn.addEventListener('click',
- () => closeModal(btn.closest(".popup"))));
+Array.from(closeBtns).forEach((btn) =>
+  btn.addEventListener("click", () => closeModal(btn.closest(".popup")))
+);
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileInputName.value;
+  profileDescription.textContent = profileInputDesc.value;
+  closeModal(popupEdit);
+}
 
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  const newCard = {
+    name: cardInputName.value,
+    link: cardInputLink.value,
+  };
+  loadImage(cardInputLink.value)
+    .then((evt) => {
+      placesList.prepend(createCard(newCard, deleteCard));
+    })
+    .catch(() => {
+      console.error("Всё идёт не по плану.");
+    });
+  evt.target.reset();
+  closeModal(popupCard);
+}
 
-const createCard = (item, deleteCard) => {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardTitle = cardElement.querySelector(".card__title");
-  const cardImage = cardElement.querySelector(".card__image");
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-
-
-  cardTitle.textContent = item.name;
-  cardImage.src = item.link;
-  cardImage.alt = item.name;
-  deleteButton.addEventListener("click", (evt) => {
-    deleteCard(evt);
-  });
-
-  return cardElement;
-};
-
-// @todo: Функция удаления карточки
-const deleteCard = (evt) => {
-  const item = evt.target.closest(".card");
-  item.remove();
-};
-
-// @todo: Вывести карточки на страницу
 const renderCards = (cards) => {
   cards.forEach((item) => {
     const cardElement = createCard(item, deleteCard);
     placesList.append(cardElement);
   });
 };
+
+function loadImage(imageUrl) {
+  return new Promise((resolve, reject) => {
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.onerror = reject;
+    image.onload = resolve;
+  });
+}
 
 renderCards(initialCards);
